9f426124263bd6a84013124951736b69328d6d77
ChangHyunkmov <hot9998@kmov.kr>
2021-02-05 13:12:21 +0900

    Apply typescript settings

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { NextComponentType } from "next";
+import { AppContext, AppInitialProps, AppProps } from "next/app";
+
+const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
